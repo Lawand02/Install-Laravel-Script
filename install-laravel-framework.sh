761cee24d383295ff3c2f31bc454bfb97a27a389
lawand02 <lawandyousef98@gmail.com>
2022-12-17 00:01:08 +0300

    some message

---
+++ install-laravel-framework.sh
@@ -0,0 +1,49 @@
+# Lawand Yousif
+
+# Laravel installation script
+
+
+
+message () {
+  echo -e "\033[0;32m$1\033[0m"
+}
+
+message "Install PHP"
+sudo apt-get update
+sudo apt-get install php
+
+message "Install php dependencies"
+sudo apt-get install php-zip
+sudo apt-get install php-mbstring
+
+message "Install composer"
+sudo apt-get install composer
+
+message "Install laravel"
+composer global require "laravel/installer"
+PATH=$PATH:$HOME/.config/composer/vendor/bin
+echo 'PATH=$PATH:$HOME/.config/composer/vendor/bin' >> $HOME/.bashrc
+
+message "Create a new Laravel project"
+laravel new laravel-app
+cd laravel-app
+
+message "Update dependencies"
+composer install
+
+message "Create environment"
+### Create initial .env file using the template file .env.example
+### Creating a security key for the application
+cp .env.example .env
+php artisan key:generate
+
+
+## message "Modify config file"
+## In laravel-app/config/app.php, change APP_DEBUG to true
+##  'debug' => env('APP_DEBUG', true),
+
+
+message "Starting local application service"
+echo "Point your browser to http://localhost:8000/"
+php artisan serve
+
